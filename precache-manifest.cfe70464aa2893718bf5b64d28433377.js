47d96c0fb4a3029f91eeb9c70d399d002f38710d
Christian Maré <chris@mare.org.uk>
2020-05-04 18:08:16 +0100

    Password test page

---
+++ precache-manifest.cfe70464aa2893718bf5b64d28433377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1052c3f261cf55148b8d4f615677adc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a078a86dca7e78749b2b",
+    "url": "/static/css/main.edb85149.chunk.css"
+  },
+  {
+    "revision": "499ab76006c4b8ba777e",
+    "url": "/static/js/2.522ce014.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.522ce014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a078a86dca7e78749b2b",
+    "url": "/static/js/main.7de7988c.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
